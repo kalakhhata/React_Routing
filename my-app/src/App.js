a19ff008f0a5a890bd80f60c39b476e32d6c31c5
kalakhhata <ompatel2501@gmail.com>
2024-10-21 11:58:13 -0400

    It was my day 1 of learning routing didn/t learn much just basic syntax and setup

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+
+export default function App(){
+  return(
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
